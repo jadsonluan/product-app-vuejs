0d45fde03938a41bd42d3a5bf6c9f498dff58413
Jadson Luan <jadsonluan1@gmail.com>
2019-04-16 07:25:34 -0300

    Change description text

--- main.js
+++ main.js
@@ -2,6 +2,6 @@ var app = new Vue({
     el: '#app',
     data: {
         product: 'Socks',
-        description: 'A nice pair of socks. 100% cotton.'
+        description: 'A pair of warm, fuzzy socks'
     }
 });
